62dc3e843490658057ff225cb8ce9e66f036e274
Alex Crichton <alex@alexcrichton.com>
2011-07-11 10:51:17 -0700

    Using nokogiri to do better parsing

--- _config/release.rb
+++ _config/release.rb
@@ -3,6 +3,7 @@
 
 require 'rubygems'
 require 'bluecloth'
+require 'nokogiri'
 
 version           = ARGV[0]
 versions_xml_file = ARGV[1]
@@ -16,13 +17,19 @@ s = s.gsub(/(class=.download. href.*Hermes-)[\d\.]+(\.zip)/, "\\1#{version}\\2")
 File.open(index, 'wb') { |f| f << s }
 
 versions = File.expand_path('../../versions.xml', __FILE__)
-new_xml = File.read(versions_xml_file).gsub("\t", '  ')
+new_xml = Nokogiri::XML(File.read(versions_xml_file).gsub("\t", '  ')).root
 
-s = File.read(versions)
-if !s.match(/Version #{version}/)
-  s = s.gsub(/(<\/language>.*\n)/, '\1' + new_xml + "\n")
-  File.open(versions, 'wb') { |f| f << s }
+s = Nokogiri::XML File.read(versions)
+has_item = s.css('item title').any? do |node|
+  if node.content == "Version #{version}"
+    node.parent.replace(new_xml)
+  end
 end
+
+if !has_item
+  s.css('language').add_next_sibling new_xml
+end
+File.open(versions, 'wb') { |f| f << s.to_xhtml(:indent => 2) }
 
 File.open(File.join(html_root, '/changelog.html'), 'wb') { |f|
   f << BlueCloth.new(File.read(changelog)).to_html
